6035b3b06a7e74352284687c3fe10964110bf1b5
Sorin Ionescu <sorin.ionescu@gmail.com>
2013-04-30 16:41:05 -0400

    Simplify the reuse of an existing gpg-agent instance

--- modules/gpg/agent.zsh
+++ modules/gpg/agent.zsh
@@ -15,18 +15,11 @@ _gpg_agent_info="$HOME/.gpg-agent-info"
 
 # Start gpg-agent.
 ps -U "$USER" -o 'command' | egrep -q '^gpg-agent( |$)' \
-  || gpg-agent \
-      --daemon \
-      --write-env-file "$_gpg_agent_info" > /dev/null
+  || gpg-agent --daemon >! "$_gpg_agent_info"
 
-# Export variables.
+# Export environment variables.
 export GPG_TTY="$(tty)"
-
-if [[ -s "$_gpg_agent_info" ]]; then
-  while read line; do
-    eval "export $line"
-  done < "$_gpg_agent_info"
-fi
+source "$_gpg_agent_info"
 
 # Clean up.
 unset _gpg_agent_info
